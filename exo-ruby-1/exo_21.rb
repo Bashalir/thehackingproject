d2b60f926465c0b32b510240fc113a14fd03d73f
bashalir <bachir@sahali.fr>
2020-07-09 15:46:11 +0200

    Add data_scientist

--- exo-ruby-1/exo_21.rb
+++ exo-ruby-1/exo_21.rb
@@ -2,5 +2,11 @@ puts "Choisis un nombre entre 1 et 25 ?"
 print "> "
 floors = gets.chomp.to_i
 puts "Voici la pyramide :"
-
-floors.downto(1) { |n| puts "#" * n }
+brick = 1
+  spaces = floors-1
+floors.times do
+  print 32.chr * spaces  # 32 is the space character
+  puts 35.chr * brick  # 35 is the '#' character
+  brick = brick + 1
+  spaces = spaces - 1
+end
